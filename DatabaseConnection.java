18f424d28c0db4f527a23d5035ceb77f165e41be
Oyoodaniel <155971702+mark-bit-hub@users.noreply.github.com>
2025-03-03 15:30:11 +0300

    Update DatabaseConnection.java
    Replace mysql password for security purposes .

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/student_registration";
     private static final String USERNAME = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = "Beracahone7245&"; // Replace with your MySQL password
+    private static final String PASSWORD = "your_password"; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
